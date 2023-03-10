4c7a17a682a26d030455ed7c357ff521bc79c172
Julián Boetto <julib_8724@hotmail.com>
2023-03-10 11:39:25 -0300

    Exchange controller creado

--- src/exchange/exchange.module.ts
+++ src/exchange/exchange.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
+import { ExchangeController } from './exchange.controller';
 
 @Module({
   providers: [ExchangeService],
-  exports: [ExchangeService]
+  exports: [ExchangeService],
+  controllers: [ExchangeController]
 })
 export class ExchangeModule { }
